4f06e592838d69e8cb1ec24c18d8555c7b70f6bf
Lancelot-d <Lancelot.d@outlook.fr>
2025-05-08 18:57:09 -0400

    Try to resolve api call error

--- dash_app/chatbot_section.py
+++ dash_app/chatbot_section.py
@@ -80,7 +80,7 @@ def update_chat(_, user_input, chat_history):
         return {"chat_history": chat_history, "loading_overlay": False, "new_value": ""}
 
     chat_history.append(f"**You:** \n{user_input}\n")
-    response = requests.get(f"http://mynetwork:8080/complete_message/?input_string={user_input}").get("completed_message", "")
+    response = requests.get(f"http://api:8080/complete_message/?input_string={user_input}").get("completed_message", "")
     chat_history.append(f"**Bot:** \n{response}\n")
     return {"chat_history": chat_history, "loading_overlay": False, "new_value": ""}
 
